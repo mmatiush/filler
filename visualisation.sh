e969754bd3ef70b6a191ade192d32854a4b51ea2
Maksym MATIUSHCHENKO <mmatiush@e2r5p3.unit.ua>
2018-05-23 21:56:02 +0300

    added visual

--- visualisation.sh
+++ visualisation.sh
@@ -3,7 +3,7 @@ magenta=$(printf "\e[45m\e[30mX\e[0m")
 light_green=$(printf "\e[102m\e[30mo\e[0m")
 light_magenta=$(printf "\e[105m\e[30mx\e[0m")
 
-sed "s|O|$green|g" |
-sed "s|X|$magenta|g" |
-sed "s|x|$light_magenta|g" |
-sed "s|o|$light_green|g"
+sed "s/O/$green/g" |
+sed "s/X/$magenta/g" |
+sed "s/x/$light_magenta/g" |
+sed "s/o/$light_green/g"
